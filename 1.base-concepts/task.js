a12333e1931a71e79df5b94e442fbc739923b581
Anna Bryzgalova <annabryzgalova1984@gmail.com>
2022-08-14 09:50:52 +0300

    Решение задач

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,18 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
+  let d = (b ** 2) - (4 * a * c);
+  if (d < 0) {
+    arr.push();
+  } else if (d == 0) {
+    let root = -b / (2 * a);
+    arr.push(root);
+  } else if (d > 0) {
+    let rootOne = (-b + Math.sqrt(d)) / (2 * a);
+    let rootTwo = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(rootOne, rootTwo);
+  }
   return arr; // array
 }
 
@@ -8,6 +20,19 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
-
-  return totalAmount;
+  if (isNaN(percent)) {
+    return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(contribution)) {
+    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(amount)) {
+    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
+  let S = amount - contribution;
+  let dateNow = new Date();
+  let diff = Date.parse(date) - Date.parse(dateNow);
+  let n = Math.ceil(diff / 1000 / 60 / 60 / 24 / 30.5);
+  let P = percent / 12 / 100;
+  let monthlyPay = S * (P + (P / (((1 + P) ** n) - 1)));
+  totalAmount = n * monthlyPay;
+  return Number(totalAmount.toFixed(2));
 }
