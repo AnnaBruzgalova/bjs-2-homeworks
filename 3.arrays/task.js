e3110afa1a287404c2f91a057b3369f080272b8a
Anna Bryzgalova <annabryzgalova1984@gmail.com>
2022-08-24 11:33:22 +0300

    Решение задачи 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = (arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]));
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter((item) => item > 0)
+    .filter((multiplicity) => multiplicity % 3 === 0)
+    .map((multiplication) => multiplication * 10)
 
   return resultArr; // array
 }
